c03aac3afe457649bdaf366fa3f7a444478daae6
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2021-04-09 18:31:57 +0200

    Validate key and value when adding a mode to LoomX object

--- loomxpy/_mode.py
+++ loomxpy/_mode.py
@@ -1,6 +1,10 @@
 from typing import MutableMapping
 from enum import Enum
 
+import pandas as pd
+import numpy as np
+from scipy import sparse
+
 from ._s7 import S7
 from ._hooks import WithInitHook
 from ._matrix import DataMatrix
@@ -44,7 +48,7 @@ class Modes(MutableMapping[str, object], metaclass=WithInitHook):
 
     def __len__(self):
         """"""
-        raise Exception("Iterator not implemented.")
+        raise Exception("Length not implemented.")
 
     def __delitem__(self, name) -> None:
         """"""
@@ -54,13 +58,57 @@ class Modes(MutableMapping[str, object], metaclass=WithInitHook):
         """"""
         return getattr(self, name)
 
-    def __setitem__(self, name, value) -> None:
-        """"""
-        print(f"DEBUG: instance call: set attr with name {name}")
-        print(f"adding new {name} mode")
-        _key = name
-        _mode = None
-        _data_matrix = None
+    def _validate_key(self, key):
+        _key = None
+        if key.startswith("_"):
+            raise Exception(
+                f"Cannot add Mode with key {key}. Not a valid key. Expects key not to start with an underscore ('_')."
+            )
+
+        if not isinstance(key, ModeType) and not str:
+            raise Exception("Not a valid ModeType.")
+
+        if isinstance(key, ModeType):
+            if key in self._mode_types:
+                _key = key
+            else:
+                raise Exception("Not a valid ModeType.")
+
+        if isinstance(key, str):
+            if key in self._mode_types:
+                _key = key
+            else:
+                raise Exception(
+                    f"Not a valid ModeType: {key}. Choose one ModeType from {', '.join(self._mode_types)}"
+                )
+        return _key
+
+    @staticmethod
+    def _validate_value(value):
+        if not type(value) in [tuple, np.matrix, pd.DataFrame]:
+            raise Exception(
+                f"""
+Got {type(value)} but expecting either:
+- Tuple: (<SciPy CSR matrix>, <feature names>, <observation names>) or
+- Tuple: (<NumPy 2D matrix>, <feature names>, <observation names>) or
+- pandas.DataFrame
+                """
+            )
+
+        if isinstance(value, sparse.csr_matrix):
+            raise Exception(
+                "If your data matrix is a SciPy CSR matrix, use tuple: (<SciPy CSR matrix>, <feature names>, <observation names>)."
+            )
+
+        if isinstance(value, sparse.csc_matrix):
+            raise Exception(
+                "If your data matrix is a SciPy CSC matrix, use tuple: (<SciPy CSC matrix>, <feature names>, <observation names>)."
+            )
+
+        if isinstance(value, np.matrix):
+            raise Exception(
+                "If your data matrix is a NumPy 2D matrix, use tuple: (<NumPy 2D matrix>, <feature names>, <observation names>)."
+            )
 
         if isinstance(value, tuple):
             if len(value) != 3:
@@ -68,11 +116,29 @@ class Modes(MutableMapping[str, object], metaclass=WithInitHook):
                     "If your data matrix is a NumPy 2D matrix or SciPy CSR matrix, use tuple: (<NumPy 2D matrix | SciPy CSR matrix>, <feature names>, <observation names>)."
                 )
 
+    def __setitem__(self, name, value) -> None:
+        """"""
+        print(f"DEBUG: instance call: set attr with name {name}")
+        print(f"adding new {name} mode")
+        _key = self._validate_key(key=name)
+        Modes._validate_value(value=value)
+
+        _mode = None
+        _data_matrix = None
+
+        if isinstance(value, tuple):
             _matrix, _feature_names, _observation_names = value
             _data_matrix = DataMatrix(
                 data_matrix=_matrix,
                 feature_names=_feature_names,
                 observation_names=_observation_names,
+            )
+
+        if isinstance(value, pd.DataFrame):
+            _data_matrix = DataMatrix(
+                data_matrix=value.values,
+                feature_names=value.columns,
+                observation_names=value.index,
             )
 
         if _data_matrix is None:
