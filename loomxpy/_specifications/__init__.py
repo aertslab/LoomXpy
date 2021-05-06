76a2373fa8534bfdd1c1f8a2af80617fc6e6e8ee
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2021-05-06 10:34:31 +0200

    Add v1 specs for LoomX metadata

---
+++ loomxpy/_specifications/__init__.py
@@ -0,0 +1,3 @@
+from .v1 import Metadata
+
+LoomXMetadata = Metadata
---
+++ loomxpy/_specifications/v1/metadata.py
@@ -0,0 +1,124 @@
+from dataclasses_json import dataclass_json
+from dataclasses import dataclass
+from typing import List
+
+GLOBAL_ATTRIBUTE_KEY = "MetaData"
+
+
+@dataclass_json
+@dataclass
+class Annotation:
+    name: str
+    values: List[str]
+
+
+@dataclass_json
+@dataclass
+class Metric:
+    name: str
+
+
+@dataclass_json
+@dataclass
+class Embedding:
+    id: int
+    name: str
+
+
+@dataclass_json
+@dataclass
+class RegulonAllThresholds:
+    tenPercentOfMax: float = None
+
+
+@dataclass_json
+@dataclass
+class RegulonThreshold:
+    regulon: str
+    defaultThresholdValue: float
+    defaultThresholdName: str
+    allThresholds: RegulonAllThresholds
+    motifData: str
+    motifVersion: Optional[str] = None
+
+
+@dataclass_json
+@dataclass
+class CellTypeAnnotationVoter:
+    voter_name: str
+    voter_id: str
+    voter_hash: str
+
+
+@dataclass_json
+@dataclass
+class CellTypeAnnotationVotes:
+    total: int
+    voters: List[CellTypeAnnotationVoter]
+
+
+@dataclass_json
+@dataclass
+class CellTypeAnnotationVotes:
+    votes_for: CellTypeAnnotationVotes
+    votes_against: CellTypeAnnotationVotes
+
+
+@dataclass_json
+@dataclass
+class CellTypeAnnotationData:
+    curator_name: str
+    curator_id: str
+    timestamp: int
+    obo_id: str
+    ols_iri: str
+    annotation_label: str
+    markers: ["ey"]
+    publication: str
+    comment: str
+
+
+@dataclass_json
+@dataclass
+class CellTypeAnnotation:
+    data: CellTypeAnnotationData
+    validate_hash: str
+    votes: CellTypeAnnotationVotes
+
+
+@dataclass_json
+@dataclass
+class ClusterMarkerMetric:
+    accessor: str
+    name: str
+    description: str
+
+
+@dataclass_json
+@dataclass
+class Cluster:
+    id: int
+    description: str
+    clusterMarkerMetrics: List[ClusterMarkerMetric] = field(default_factory=lambda: [])
+    cell_type_annotation: List[CellTypeAnnotation] = field(default_factory=lambda: [])
+
+
+@dataclass_json
+@dataclass
+class Clustering:
+    id: int
+    group: str
+    name: str
+    clusters: List[Cluster]
+
+
+@dataclass_json
+@dataclass
+class Metadata:
+    metrics: Optional[List[Metric]] = field(default_factory=lambda: [])
+    annotations: Optional[List[Annotation]] = field(default_factory=lambda: [])
+    embeddings: Optional[List[Embedding]] = field(default_factory=lambda: [])
+    regulonThresholds: Optional[List[RegulonThreshold]] = field(
+        default_factory=lambda: []
+    )
+    clusterings: Optional[List[Clustering]] = field(default_factory=lambda: [])
