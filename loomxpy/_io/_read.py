462ac90f869e3d69018b166ec9b9d56d3fc41d34
dweemx <maxime.dewaegeneer@kuleuven.vib.be>
2021-05-06 16:43:01 +0200

    Read and write (o) annotations and (o) metrics to LoomX

--- loomxpy/_io/_read.py
+++ loomxpy/_io/_read.py
@@ -1,17 +1,17 @@
 import json
 import zlib
 import base64
+import pandas as pd
 import numpy as np
 import loompy as lp
-from typing import NamedTuple
+from typing import NamedTuple, Dict
 from scipy import sparse
 
 from loomxpy._loomx import LoomX
-from loomxpy._mode import ModeType
-from loomxpy._specifications import LoomXMetadata
+from loomxpy._mode import Mode, ModeType
+from loomxpy._specifications import LoomXMetadata, GLOBAL_ATTRIBUTE_KEY
 
 
-@staticmethod
 def decompress_metadata(metadata: str):
     try:
         metadata = metadata.decode("ascii")
@@ -22,18 +22,21 @@ def decompress_metadata(metadata: str):
         )
 
 
-@staticmethod
 def get_metadata_from_loom_connection(loom_connection: lp.LoomConnection):
-    _metadata = loom_connection.attrs.MetaData
-    if type(md) is np.ndarray:
-        _metadata = loom_connection.attrs[GLOBAL_ATTRIBUTE_KEY][0]
+    _metadata = loom_connection.attrs[GLOBAL_ATTRIBUTE_KEY]
+    if type(_metadata) is np.ndarray:
+        _metadata = _metadata[0]
     try:
         return json.loads(s=_metadata)
     except json.decoder.JSONDecodeError:
         return decompress_metadata(metadata=_metadata)
 
 
-def _read_scope_rna_loom(loom_connection: lp.LoomConnection):
+def _read_scope_rna_loom(loom_connection: lp.LoomConnection, force_conversion: Dict):
+    # Read the Metadata from SCope
+    _metadata: LoomXMetadata = LoomXMetadata.from_dict(
+        get_metadata_from_loom_connection(loom_connection=loom_connection)
+    )
     # Loompy stores the features as rows and the observations as columns
     # LoomX follows Scanpy convention (i.e.: machine learning/statistics standards) i.e.: observations as rows and the features as columns. This requires to transpose the matrix
     _matrix = loom_connection[:, :]
@@ -49,20 +52,61 @@ def _read_scope_rna_loom(loom_connection: lp.LoomConnection):
         loom_connection.ra["Gene"],
         loom_connection.ca["CellID"],
     )
+    ## Add observation (o) attributes
+    ### Add annotations
+    try:
+        for annotation in _metadata.annotations:
+            lx.modes.rna.o.annotations.add(
+                key=annotation.name,
+                value=pd.Series(
+                    data=loom_connection.ca[annotation.name],
+                    index=lx.modes.rna.X._observation_names,
+                    name=annotation.name,
+                ),
+                force=force_conversion["annotations"]
+                if "annotations" in force_conversion
+                else False,
+            )
+    except:
+        raise Exception(
+            "You can force the conversion of the annotations to categorical by setting the parameter `force_conversion={'annotations': True}`"
+        )
+    ### Add metrics
+    try:
+        for metric in _metadata.metrics:
+            lx.modes.rna.o.metrics.add(
+                key=metric.name,
+                value=pd.Series(
+                    data=loom_connection.ca[metric.name],
+                    index=lx.modes.rna.X._observation_names,
+                    name=metric.name,
+                ),
+                force=force_conversion["metrics"]
+                if "metrics" in force_conversion
+                else False,
+            )
+    except:
+        raise Exception(
+            "You can force the conversion of the metrics to numerical by setting the parameter `force_conversion={'metrics': True}`"
+        )
     return lx
 
 
-def _read_scope_loom(loom_connection: lp.LoomConnection, mode_type: ModeType):
-    # Read the Metadata from SCope
-    _metadata: LoomXMetadata = LoomXMetadata.from_dict(
-        get_metadata_from_loom_connection(loom_connection=loom_connection)
-    )
+def _read_scope_loom(
+    loom_connection: lp.LoomConnection, mode_type: ModeType, force_conversion: Dict
+):
     if mode_type == ModeType.RNA:
-        return _read_scope_rna_loom(loom_connection=loom_connection)
+        return _read_scope_rna_loom(
+            loom_connection=loom_connection, force_conversion=force_conversion
+        )
     raise Exception(f"The given mode type '{mode_type}' has not been implemented yet.")
 
 
-def read_loom(file_path: str, mode_type: str = "rna") -> LoomX:
+def read_loom(
+    file_path: str,
+    mode_type: str = "rna",
+    force_conversion={"annotations": False, "metrics": False},
+) -> LoomX:
 
     try:
         _mode_type = ModeType(mode_type)
@@ -75,6 +119,8 @@ def read_loom(file_path: str, mode_type: str = "rna") -> LoomX:
     with lp.connect(filename=file_path, mode="r", validate=False) as loom_connection:
         if GLOBAL_ATTRIBUTE_KEY in loom_connection.attrs:
             return _read_scope_loom(
-                loom_connection=loom_connection, mode_type=_mode_type
+                loom_connection=loom_connection,
+                mode_type=_mode_type,
+                force_conversion=force_conversion,
             )
         raise Exception(f"Unable to read the loom at {file_path}")
